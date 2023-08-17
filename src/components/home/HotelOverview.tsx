dba52bab9070d8e788b1fcd9bf90cf28f5e30aa9
Niklas Röske <n.roeske@ostfalia.de>
2023-08-17 17:01:01 +0200

    create HotelOverview to show information of hotel

---
+++ src/components/home/HotelOverview.tsx
@@ -0,0 +1,24 @@
+const HotelOverview = () => {
+  return (
+    <div className="h-1/2 flex text-center">
+      <div className="w-2/3  ">
+        <p className="py-3 px-16">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+      <div className="w-1/3 flex items-center">
+        <p className="text-3xl">The Retreat</p>
+      </div>
+    </div>
+  );
+};
+
+export default HotelOverview;
