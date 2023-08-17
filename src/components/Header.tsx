ccd70868e268593f1cc16992cedbdf332bd93f93
Niklas Röske <n.roeske@ostfalia.de>
2023-08-17 17:00:30 +0200

    create header and footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+
+
+    return (
+        <div className="shadow-2xl bg-sky-500 border-b border-sky-500 p-3 w-full flex fixed z-20">
+            <a className="mx-auto text-white h-10 text-2xl align-baseline" href="/">
+                The Retreat
+            </a>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/home/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+    return (
+        <div className="h-20 bg-sky-500 mt-16 p-8 text-white">
+            © 2023 The Retreat Hotels
+        </div>
+    );
+}
+
+export default Footer;
