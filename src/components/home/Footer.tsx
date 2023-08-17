92887dddfb5d106299eba0357fbe50382e50175c
Niklas Röske <n.roeske@ostfalia.de>
2023-08-17 17:05:11 +0200

    change padding of Footer

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 
     return (
-        <div className="h-20 bg-sky-500 mt-16 p-8 text-white">
+        <div className=" bg-sky-500 p-3 text-white">
             © 2023 The Retreat Hotels
         </div>
     );
